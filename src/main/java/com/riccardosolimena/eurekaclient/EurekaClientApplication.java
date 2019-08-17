02aef6d2b28ed465c8296c919d8d9d7f9d566408
riccardosolimena <riccardo.solimena.fc@student.supsi.ch>
2019-08-17 23:21:38 +0200

    Initial commit

---
+++ src/main/java/com/riccardosolimena/eurekaclient/EurekaClientApplication.java
@@ -0,0 +1,26 @@
+package com.riccardosolimena.eurekaclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class EurekaClientApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaClientApplication.class, args);
+    }
+
+}
+
+@RestController
+class ClassRestController {
+
+    @RequestMapping("/")
+    public String welcome(){
+        return "benvenuto!";
+    }
+}
